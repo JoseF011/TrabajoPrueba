75c3a8ad1593dd27ee023519cc882972a1bb29f0
Jose Galo <fjng01234567812@gmail.com>
2022-09-10 20:54:56 -0600

    Se agrega una prueba para trabajar en equipos

---
+++ README.md
@@ -0,0 +1,1 @@
+## Probando si se puede ir agregando trabajos
---
+++ src/main/java/com/mycompany/prueba/prueba.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.prueba;
+import java.util.Scanner;
+/**
+ *
+ * @author luisa
+ */
+public class prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner(System.in);
+        int variable1;
+        
+        System.out.println("Escirba el numero: ");
+        variable1=entrada.nextInt();
+        System.out.println("Variable: "+variable1);
+    }
+    
+}
